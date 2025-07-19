64204ff960e7b35d21243a924c9a028a63d78ed0
MohitKumar37 <mohitbairwa8476@gmail.com>
2025-07-19 23:16:11 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("");
+
+  return (
+    <div
+      className="w-full h-screen duration-1000"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow- bg-black px-3 py-2 rounded-2xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("yellow")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
+          <button
+            onClick={() => setColor("olive")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "olive" }}
+          >
+            Olive
+          </button>
+          <button
+            onClick={() => setColor("maroon")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "maroon" }}
+          >
+            Maroon
+          </button>
+          <button
+            onClick={() => setColor("lime")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "lime" }}
+          >
+            Lime
+          </button>
+          <button
+            onClick={() => setColor("olive")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "olive" }}
+          >
+            Olive
+          </button>
+          <button
+            onClick={() => setColor("teal")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "teal" }}
+          >
+            Teal
+          </button>
+          <button
+            onClick={() => setColor("aqua")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "aqua" }}
+          >
+            Aqua
+          </button>
+          <button
+            onClick={() => setColor("silver")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "silver" }}
+          >
+            Silver
+          </button>
+          <button
+            onClick={() => setColor("gold")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "gold" }}
+          >
+            Golden
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
